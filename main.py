36319c922baf0df8612d17abc5c183bf7c4cb28f
feagy <ferhat1903dnz@gmail.com>
2026-07-22 22:27:38 +0300

    calculator scripts added

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ api_key = os.environ.get("OPENROUTER_API_KEY")
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 
 if api_key is None:
@@ -30,8 +31,9 @@ response = client.chat.completions.create(
                 model = "openrouter/free",
                 messages = messages    
             )
-
-print(f"Prompt tokens: {response.usage.prompt_tokens}")
-print(f"Response tokens: {response.usage.completion_tokens}")
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage.prompt_tokens}")
+    print(f"Response tokens: {response.usage.completion_tokens}")
 print("Response:")
 print(response.choices[0].message.content)
